327ff26f47447e7bd51bdf0bc516e98ab1a2836f
Trevor F. Smith <trevor@trevor.smith.name>
2018-03-05 17:41:08 -0800

    Updated pseudocode based on feedback.

--- src/example/Simplest.js
+++ src/example/Simplest.js
@@ -1,58 +1,69 @@
 import ActionManager from "../action/ActionManager.js";
 import DoubleJumpFilter from "./filters/DoubleJumpFilter.js";
 
-function main() {
-  // App specific setup of the ActionManager
+/*
+This is some pseudocode of the kind of app-specific setup of the ActionManager
+which would happen within an app framework like React or in a scene framework like A-frame.
+*/
 
+function calledDuringSetup() {
   let actionManager = new ActionManager();
 
-  // Load a custom filter
-  actionManager.loadFilter(DoubleJumpFilter, "/filter/double-jump");
+  // There are already some installed filters provided by the action-input lib
+  actionManager.filters;
+  /*
+    returns {
+      "/filter/lib/reverse-axis": ReverseAxisFilter,
+      "/filter/lib/low-pass": LowPassFilter,
+      ...
+    }
+  */
 
-  // Load up the maps that bind inputs to filters and actions
+  // Let's load an app-specific filter that watches thumbpad motion and emits app-specific actions
+  actionManager.loadFilter("thumb-swirl", new example.ThumbSwirlRecognizerFilter()); // now resolves at "/filter/app/thumb-swirl"
+
+  // Load up the maps that bind inputs to filters aor actions. Also, give them action set names.
+  // Perhaps this would be better as a simple actionManager.loadActionSets(<url to single JSON>) ?
   actionManager
-    .loadActionMaps(
-      // Maps used while the mode is flat
-      "./flat-playing.map",
-      "./flat-paused.map",
-      "./flat-menu.map",
-      // Maps used while the mode is portal
-      "./portal-playing.map",
-      "./portal-paused.map",
-      "./portal-menu.map",
-      // Maps used while the mode is immersive
-      "./immersive-playing.map",
-      "./immersive-paused.map",
-      "./immersive-menu.map"
-    )
-    .then(maps => {
-      console.log("Maps are loaded", maps);
+    .loadActionSets({
+      "flat-playing": "./flat-playing-map.json",
+      "flat-paused": "./flat-paused-map.json",
+      "flat-menu": "./flat-menu-map.json",
+      "portal-playing": "./portal-playing-map.json",
+      "portal-paused": "./portal-paused-map.json",
+      "portal-menu": "./portal-menu-map.json",
+      "immersive-playing": "./immersive-playing-map.json",
+      "immersive-paused": "./immersive-paused-map.json",
+      "immersive-menu": "./immersive-menu-map.json"
+    })
+    .then(actionSets => {
+      console.log("Action sets are loaded", actionSets);
     })
     .catch(err => {
-      console.log("Could not load action maps", err);
+      console.log("Could not load action sets", err);
     });
 
   /*
-	While the app is running, it has to provide logic for [de]activating maps as
-	the mode changes between flat, portal, and immersive and as the app changes state like from playing
-	to paused to using a config menu
-	*/
-  actionManager.activateMapsExclusive("/map/flat-playing");
+  While the app is running, it has to provide logic for [de]activating action sets as
+  the display mode changes between flat, portal, and immersive and as the app changes situations
+  between playing to paused to using a config menu
+  */
+  actionManager.switchToActionSet("/action-set/flat-playing");
   // or
-  actionManager.activateMapsExclusive("/map/portal-paused");
+  actionManager.switchToActionSet("/action-set/portal-paused");
   // or
-  actionManager.activateMapsExclusive("/map/immersive-menu");
+  actionManager.switchToActionSet("/action-set/immersive-menu");
   // etc
 
   // The app can subscribe to action events
-  actionManager.addListener((event, ...params) => {
+  actionManager.addActionListener("/action/jump", (event, ...params) => {
     console.log("Jump event", event, ...params);
-  }, "/action/jump");
+  });
 
   // The app can subscribe to action events using semantic path wildcards
-  actionManager.addListener((event, ...params) => {
+  actionManager.addActionListener("/action/menu/*", (event, ...params) => {
     console.log("Menu action event", event, ...params);
-  }, "/action/menu/*");
+  });
 
   // The app can poll for action state
   actionManager.isActive("/action/jump"); // returns bool
@@ -60,12 +71,12 @@ function main() {
   // Or, if the app needs to poll for action-specific info:
   actionManager.get("/action/jump");
   /*
-		returns
-		{
-			action: Action,
-			active: bool,
-			isDoubleJump: bool,
-			other action specific params...
-		}
-	*/
+    returns
+    {
+      action: Action,
+      active: bool,
+      isDoubleJump: bool,
+      other action specific params...
+    }
+  */
 }
